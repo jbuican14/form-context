7949776cea6139114799fc608484771208d2a9d2
Juti Buican <juti.buican@bottomline.com>
2023-04-24 14:52:42 +0100

    Create a new Navbar components with Material UI (issue with theme's value)

---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import {AppBar, Toolbar, IconButton, Typography, Switch, InputBase } from "@material-ui/core"
+import { withStyles } from '@material-ui/styles';
+import { SearchRounded } from '@material-ui/icons';
+import styles from '../styles/NavBarStyles';
+
+function Navbar(props) {
+  console.log('🚀 ~ file: Navbar.js:6 ~ Navbar ~ props:', props)
+  const {classes} = props;
+  console.log('🚀 ~ file: Navbar.js:8 ~ Navbar ~ props:', classes)
+  return (
+    <div className={classes.root}>
+      <AppBar position='static' color='primary'>
+        <Toolbar>
+          <IconButton className={classes.menuButton} color="inherit">
+            <span>🇫🇷</span>
+          </IconButton>
+          <Typography className={classes.title} variant='h1' color='inherit'>App Title</Typography>
+          <Switch />
+          <div className={classes.grow}/>
+          <div className={classes.search}>
+            <div className={classes.SearchRounded}>
+              <SearchRounded />
+              </div>
+              <InputBase placeholder='Search...' classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput
+              }} />
+            
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
+
+export default withStyles(styles)(Navbar); 
---
+++ src/styles/NavBarStyles.js
@@ -0,0 +1,67 @@
+import { Fade } from '@material-ui/core'
+const styles = theme => {
+console.log('🚀 ~ file: NavBarStyles.js:3 ~ styles ~ theme:', theme.breakpoints)
+return {
+  root: {
+    width: "100%",
+    marginBottom: 0,
+
+  }, 
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  },
+  title: {
+    display: "none",
+    [theme.breakpoints]: {
+      display: "block"
+    }
+  },
+  search: {
+    position: "relative",
+    // borderRadius: theme.shape.borderRadius,
+    // backgroundColor: Fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      // backgroundColor: Fade(theme.palette.common.white, 0.25)
+    },
+    marginLeft: 0,
+    width: "100%",
+    // [theme.breakpoints.up("sm")]: {
+    //   marginLeft: theme.spacing.unit,
+    //   width: "auto"
+    // }
+  },
+  SearchRounded: {
+    // width: theme.spacing * 9,
+    width: 10 * 9,
+    height: "100%",
+    position: "absolute",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+    width: "100%"
+  },
+  inputInput: {
+    paddingTop: 20,
+    paddingRight: 20,
+    paddingBottom: 20,
+    paddingLeft: 6* 10,
+    transition: "width",
+    width: "100%",
+    // [theme.breakpoints.up("sm")]: {
+    //   width: 120,
+    // "&:focus" {
+      // width: 200,
+    // }
+    // }
+
+  }
+};
+}
+export default styles;
