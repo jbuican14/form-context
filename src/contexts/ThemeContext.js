30d4262390c79d8d6e88c650f49e4dcffb47152f
Juti <jbuican19@gmail.com>
2023-06-04 18:00:34 +0100

    Update ThemeContext.js
    import and use custom hook

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,15 +1,12 @@
-import {createContext, useState} from 'react'
+import {createContext} from 'react';
+import useToggeState from '../hooks/useToggleState';
 
 export const ThemeContext = createContext();
 
 export function ThemeProvider(props) {
 
   // eslint-disable-next-line no-unused-vars
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  const toggleTheme = () => {
-    setIsDarkMode(!isDarkMode)
-  }
+  const [isDarkMode, toggleTheme] = useToggeState(false);
 
   return (
     <ThemeContext.Provider value={{isDarkMode, toggleTheme}}>
